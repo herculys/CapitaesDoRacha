5dd99e031831a3615b2e82fd81bbd4601c6c0022
Thiago <Hall-9000@DESKTOP-BA63S82.Home>
2017-02-12 10:07:58 -0300

    Criando a Classe Time

---
+++ CapitaesDoRacha/src/capitaesdoracha/Time.java
@@ -0,0 +1,14 @@
+package capitaesdoracha;
+
+public class Time 
+{
+    private String Nome;
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String _nome) {
+        this.Nome = _nome;
+    }
+}
