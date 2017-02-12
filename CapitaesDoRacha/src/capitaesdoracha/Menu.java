cf460d707d10b53da379780233eddd675db19151
Thiago <Hall-9000@DESKTOP-BA63S82.Home>
2017-02-12 12:20:00 -0300

    Criado os metodos de Menu pricipal e de jogos

--- CapitaesDoRacha/src/capitaesdoracha/Menu.java
+++ CapitaesDoRacha/src/capitaesdoracha/Menu.java
@@ -1,6 +1,25 @@
 package capitaesdoracha;
 
-
-public class Menu {
+public class Menu 
+{
+    public void MenuPricipal()
+    {
+        System.out.println("===================================");
+        System.out.println("########## Menu Pricipal ##########");
+        System.out.println("1 - Adicionar Jogadores");
+        System.out.println("2 - Adicionar Nome aos Times");
+        System.out.println("3 - Jogos");
+        System.out.println("===================================");
+        
+    }
     
+    public void MenuJogos()
+    {
+        System.out.println("===================================");
+        System.out.println("########## Menu Jogos ##########");
+        System.out.println("1 - Zero ou Um");
+        System.out.println("2 - Impar ou Par");
+        System.out.println("3 - Voltar");
+        System.out.println("===================================");
+    }
 }
