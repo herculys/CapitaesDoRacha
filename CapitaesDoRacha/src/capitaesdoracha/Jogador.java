370e77716b88937e922953f58e4e31b9d10b9d12
Thiago <Hall-9000@DESKTOP-BA63S82.Home>
2017-02-12 10:07:41 -0300

    Criando a Classe Jogador

---
+++ CapitaesDoRacha/src/capitaesdoracha/Jogador.java
@@ -0,0 +1,25 @@
+package capitaesdoracha;
+
+public class Jogador 
+{
+    private String Nome;
+    private Time Time;
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String _nome) {
+        this.Nome = _nome;
+    }
+
+    public Time getTime() {
+        return Time;
+    }
+
+
+    public void setTime(Time _time) {
+        this.Time = _time;
+    }
+    
+}
