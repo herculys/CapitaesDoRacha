e1901180cce1f246fc97466a2871ee0d5581e090
Thiago <Hall-9000@DESKTOP-BA63S82.Home>
2017-02-12 20:00:06 -0300

    Removendo comentarios

--- CapitaesDoRacha/src/capitaesdoracha/JogadorComparator.java
+++ CapitaesDoRacha/src/capitaesdoracha/JogadorComparator.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package capitaesdoracha;
 
 import java.util.Comparator;
 
-/**
- *
- * @author Hall-9000
- */
 public class JogadorComparator implements Comparator<Jogador>
 {
 
