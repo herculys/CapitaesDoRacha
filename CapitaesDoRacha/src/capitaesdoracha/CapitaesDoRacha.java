89916465f9d1b8e1ac57aa6dc9e2a428af87e4c4
Thiago <Hall-9000@DESKTOP-BA63S82.Home>
2017-02-12 15:09:38 -0300

    Deixado pronto o menu pricipal

--- CapitaesDoRacha/src/capitaesdoracha/CapitaesDoRacha.java
+++ CapitaesDoRacha/src/capitaesdoracha/CapitaesDoRacha.java
@@ -10,6 +10,7 @@ public class CapitaesDoRacha
     static Jogador jogador3 = new Jogador();
     static Time TimeA =  new Time();
     static Time TimeB = new Time();
+    static Menu menu = new Menu();
     
     
     public static void AdicionarNomesJogadores()
@@ -40,13 +41,69 @@ public class CapitaesDoRacha
     
     public static void EntradasZeroOuUm()
     {
+        Scanner entrada = new Scanner(System.in);
         
+        System.out.println("============ Zero ou Um ===========");
+        System.out.println("Digite a escolha do jogadaor 1: ");
+        jogador1.setEscolha(entrada.nextInt());
+        System.out.println("Digite a escolha jogadaor 2: ");
+        jogador2.setEscolha(entrada.nextInt());
+        System.out.println("Digite a escolha jogadaor 3: ");
+        jogador3.setEscolha(entrada.nextInt());
+        System.out.println();
+        System.out.println("Escolhas Adicionadas");
+    }
+    
+    public static void EntradasImparOuPar()
+    {
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("Digite um valor entre 0 e 10: ");
+        jogador1.setEscolha(entrada.nextInt());
+        System.out.println("Digite um valor entre 0 e 10: ");
+        jogador2.setEscolha(entrada.nextInt()); 
+        System.out.println();
+        System.out.println("Escolhas Adicionadas");
+    }
+    
+    public static void EscolherImparOuPar()
+    {
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("Digite sua escolha: ");
+        SalvandoEscolha(entrada.nextInt(), jogador1, jogador2);
+        System.out.println(""); 
+        System.out.println("Escolhas Adicionadas");
+    }
+    
+    public static void SalvandoEscolha(int escolha, Jogador _jogador1, Jogador _jogador2)
+    {
+        if(escolha == 1)
+        {
+            _jogador1.setCategoria(ImparOuPar.IMPAR);
+            _jogador2.setCategoria(ImparOuPar.PAR);
+            System.out.println("O Jogador 2 Ficou com a opção PAR");
+            System.out.println("");
+        }
+        else
+        {
+            _jogador1.setCategoria(ImparOuPar.PAR);
+            _jogador2.setCategoria(ImparOuPar.IMPAR);
+            System.out.println("O Jogador 2 Ficou com a opção ÍMPAR");
+            System.out.println("");
+        }
+    }
+    
+    public static void menuImparOuPar()
+    {
+        System.out.println("##### Escolhendo os Capitães ######");
+        EntradasZeroOuUm();
+        menu.MenuImparOuPar();
+        EscolherImparOuPar();
     }
 
     public static void main(String[] args) 
     {
-
-        Menu menu = new Menu();
         boolean loop = true;
         
         do
@@ -66,7 +123,7 @@ public class CapitaesDoRacha
                         AdicionarNomesTimes();
                         break;
                     case 3:
-                        
+                        menuImparOuPar();
                         break;
                     case 4:
                         loop = false;
