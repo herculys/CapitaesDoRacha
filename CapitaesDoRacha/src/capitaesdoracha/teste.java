cb66ce54f21bc67c2d21114b76f39a34d0fea3ea
Thiago <Hall-9000@DESKTOP-BA63S82.Home>
2017-02-12 14:16:16 -0300

    Classe para testes isolados

---
+++ CapitaesDoRacha/src/capitaesdoracha/teste.java
@@ -0,0 +1,29 @@
+package capitaesdoracha;
+
+
+public class teste 
+{
+    public static void main(String[] args)
+    {
+        Jogador jogador1 = new Jogador();
+        Jogador jogador2 = new Jogador();
+        Jogador jogador3 = new Jogador();
+        Time TimeA =  new Time();
+        Time TimeB = new Time();
+        Jogos jogos = new Jogos();
+        
+        jogador1.setCategoria(ImparOuPar.IMPAR);
+        jogador1.setNome("Thiago");
+        jogador1.setEscolha(0);
+        jogador2.setCategoria(ImparOuPar.IMPAR);
+        jogador2.setNome("Filipe");
+        jogador2.setEscolha(1);
+        jogador3.setNome("João");
+        jogador3.setEscolha(0);
+        
+        Jogador ganhador = jogos.ZeroOuUm(jogador1, jogador2, jogador3);
+        
+        System.out.println(ganhador.getNome());
+        
+    }
+}
