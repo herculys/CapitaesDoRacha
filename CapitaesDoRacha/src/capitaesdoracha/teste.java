47375c01029348beab70bc096f02816c84db11d5
Thiago <Hall-9000@DESKTOP-BA63S82.Home>
2017-02-12 22:11:21 -0300

    Novos testa para os metodos da classe jogos

--- CapitaesDoRacha/src/capitaesdoracha/teste.java
+++ CapitaesDoRacha/src/capitaesdoracha/teste.java
@@ -12,18 +12,23 @@ public class teste
         Time TimeB = new Time();
         Jogos jogos = new Jogos();
         
-        jogador1.setCategoria(ImparOuPar.IMPAR);
+        jogador1.setCategoria(ImparOuPar.PAR);
         jogador1.setNome("Thiago");
-        jogador1.setEscolha(1);
+        jogador1.setEscolha(3);
         jogador2.setCategoria(ImparOuPar.IMPAR);
         jogador2.setNome("Filipe");
-        jogador2.setEscolha(1);
+        jogador2.setEscolha(4);
         jogador3.setNome("João");
-        jogador3.setEscolha(1);
+        jogador3.setEscolha(0);
         
-        Jogador ganhador = jogos.ZeroOuUm(jogador1, jogador2, jogador3);
+        System.out.println(jogador1.getNome() + " e " + jogador2.getNome());
         
+        Jogador ganhador = jogos.ImparOuPar(jogador1, jogador2);
         System.out.println(ganhador.getNome());
+        
+        ganhador = jogos.ZeroOuUm(jogador1, jogador2, jogador3);        
+        System.out.println(ganhador.getNome());
+
         
     }
 }
